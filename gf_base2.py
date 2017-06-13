4780ffacae56f43f9eecdda996dcb9c4319b419a
Sherman Yin <shermanyin@gmail.com>
2017-06-12 17:14:36 -0700

    Change exit(1) to Exception
    Should have raised exception instead of exiting when moving code to the
    gf2 class.
    
    Signed-off-by: Sherman Yin <shermanyin@gmail.com>

--- gf_base2.py
+++ gf_base2.py
@@ -7,14 +7,17 @@ class gf2:
            the coefficients only.  eg. 11 for m=3 (x^3+x+1), or 283 for m=8.
            Highest degree must be m.
         """
+        # g(x) must be of degree m
+        if ((g >> m) != 1):
+            msg = (
+                'Incorrect value for g(x) = {}.  Highest degree of g(x) must '
+                'be equal to m ({}). Initialization failed.'.format(g, m)
+            )
+            raise ValueError(msg)
+
         self.m = m
         self.g = g
 
-        # g(x) must be of degree m
-        if ((g >> m) != 1):
-            print 'Incorrect g(x)={} for degree m={}:'.format(g, m)
-            print 'Initialization failed.'
-            exit(1)
 
     def add(self, x, y):
         return x ^ y
